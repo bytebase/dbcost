9ea8b6f8761be3453db7423e377a1f2b97fcc10d
Zilong Zhou <529620861@qq.com>
2022-04-16 10:10:49 +0800

    add icon plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import vue from "@vitejs/plugin-vue";
+
+const r = (...args: string[]) => resolve(__dirname, ...args);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    Components({
+      dirs: [r("src/components")],
+      // auto import icons
+      resolvers: [
+        IconsResolver({
+          prefix: "",
+        }),
+      ],
+    }),
+    Icons(),
+  ],
+});
