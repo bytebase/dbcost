6c8f86559697b73d10780d203aaffe5be52750e1
Zilong Zhou <529620861@qq.com>
2022-04-22 14:58:21 +0800

    tweak foother and header

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,24 +1,45 @@
 <template>
-  <div class="px-5 pt-5 items-center">
-    <img
-      class="inline w-6 h-6 align-text-bottom"
-      src="../assets/icon/logo-icon.svg"
-    />
-    <span class="inline font-bold text-xl text-center ml-2">DB COST</span>
+  <div class="px-5 pt-4 items-center">
+    <span class="inline font-bold text-xl text-center ml-2">DB Cost</span>
 
-    <div class="inline align-middle float-right">
-      <div v-is="'script'" async src="https://buttons.github.io/buttons.js" />
-      <a
-        class="github-button"
-        href="https://github.com/bytebase/dbcost"
-        data-color-scheme="no-preference: light; light: light; dark: dark;"
-        data-size="middle"
-        data-show-count="true"
-        aria-label="Star bytebase/dbcost on GitHub"
-        >Star</a
-      >
+    <div class="flex items-center float-right space-x-4">
+      <div class="">
+        <div v-is="'script'" async src="https://buttons.github.io/buttons.js" />
+        <a
+          class="github-button"
+          href="https://github.com/bytebase/dbcost"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star bytebase/dbcost on GitHub"
+          >Star</a
+        >
+      </div>
+
+      <div class="flex items-center pb-1.5">
+        <span>Made By</span>
+        <a href="https://bytebase.com" target="_blank">
+          <img
+            class="h-7 ml-2"
+            src="../assets/icon/logo-icon.svg"
+            alt="Bytebase"
+          />
+        </a>
+        <div class="ml-1">
+          <n-tooltip trigger="hover" :show-arrow="false">
+            <template #trigger>
+              <heroicons-outline:information-circle class="h-4 text-gray-600" />
+            </template>
+            <template #default>
+              <span>Learn More About Bytebase</span>
+            </template>
+          </n-tooltip>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NTooltip } from "naive-ui";
+</script>
