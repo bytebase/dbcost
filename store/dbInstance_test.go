171472aa7a664d32d1541e59cb027a47d45b0a58
Zilong Zhou <529620861@qq.com>
2022-04-23 18:34:25 +0800

    removed useless code

--- store/dbInstance_test.go
+++ store/dbInstance_test.go
@@ -39,35 +39,6 @@ func Test_AWSSaveToLocal(t *testing.T) {
 	}
 }
 
-// func Test_GCPSaveToLocal(t *testing.T) {
-
-// 	offerList, instanceList, err := gcp.GetOfferInstance()
-// 	if err != nil {
-// 		t.Fatalf("fail to get price instance info, [internal error]: %v", err)
-// 	}
-// 	dbInstanceList, err := ConvertGCP(offerList, instanceList)
-// 	if err != nil {
-// 		t.Fatalf("fail to convert to dbInstance, [internal error]: %v", err)
-// 	}
-
-// 	dirPath := fmt.Sprintf("./data/test")
-// 	if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
-// 		t.Fatalf("fail to mkdir, [internal error]: %v", err)
-// 	}
-// 	filePath := fmt.Sprintf("%s/gcp-full.json", dirPath)
-// 	fd, err := os.Create(filePath)
-// 	if err != nil {
-// 		t.Fatalf("fail to mk file, [internal error]: %v", err)
-// 	}
-// 	dataByted, err := json.Marshal(dbInstanceList)
-// 	if err != nil {
-// 		t.Fatalf("fail to marshal, [internal error]: %v", err)
-// 	}
-// 	if _, err := fd.Write(dataByted); err != nil {
-// 		t.Fatalf("fail to write to file, [internal error]: %v", err)
-// 	}
-// }
-
 func Test_Replace(t *testing.T) {
 	a := strings.Replace("SQLGen2InstancesN1Standard", "SQLGen2Instances", "", -1)
 	fmt.Println(a)
