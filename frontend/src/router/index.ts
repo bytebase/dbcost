83a201af1c6a8fc52362a2d0bc152ca3571d3b22
Zilong Zhou <529620861@qq.com>
2022-04-29 11:44:16 +0800

    sync URL param and store

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import App from "../layout/AppLayout.vue";
+import { useSearchConfigStore } from "../stores/searchConfig";
+import { ChargeType, CloudProvider, EngineType } from "../types";
 
-export type routeParam = {
-  provider: string;
-  engine: string;
-  region: string;
+export type RouteParam = {
+  cloudProvider: string;
+  engineType: string;
   chargeType: string;
+  region: string;
   minCPU: number;
   minRAM: number;
   keyword: string;
 };
-
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
@@ -33,6 +34,26 @@ export const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  // directly access index page
+  if (to.fullPath === "/") {
+    next();
+    return;
+  }
+
+  // typed in URL with param
+  if (!from.name) {
+    const config = useSearchConfigStore().searchConfig;
+    const query = to.query;
+
+    config.cloudProvider = query.cloudProvider as CloudProvider;
+    config.region = (query.region as string).split("-");
+    config.chargeType = (query.chargeType as string).split("-") as ChargeType[];
+    config.engineType = (query.engineType as string).split("-") as EngineType[];
+    config.keyword = query.keyword as string;
+    config.minCPU = Number(query.minCPU as string);
+    config.minRAM = Number(query.minRAM as string);
+  }
+
   next();
 });
 
