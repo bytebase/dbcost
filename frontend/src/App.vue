356a81ceaa7203c282556a8121a5a0a5037ed43d
Zilong Zhou <529620861@qq.com>
2022-05-05 10:46:40 +0800

    add notification

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <router-view />
+  <n-notification-provider :max="2">
+    <router-view />
+  </n-notification-provider>
 </template>
+
+<script lang="ts" setup>
+import { NNotificationProvider } from "naive-ui";
+</script>
