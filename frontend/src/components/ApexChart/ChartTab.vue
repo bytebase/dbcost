bf286396006d8f436ec823fa32319e1aa6ad268a
Zilong Zhou <529620861@qq.com>
2022-05-17 16:35:34 +0800

    bar chart

--- frontend/src/components/ApexChart/ChartTab.vue
+++ frontend/src/components/ApexChart/ChartTab.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="justify-center flex items-center">
-    <spec-bubble-chart
-      class="max-w-xl w-1/2"
-      ref="bubbleChart"
-      :data="data"
-      @legend-click="handleLegendClick"
-    />
-
-    <cost-line-chart class="max-w-lg w-1/2" ref="lineChart" :data="data" />
+  <div>
+    <div class="justify-center flex items-center">
+      <spec-bubble-chart
+        class="max-w-xl w-1/2"
+        ref="bubbleChart"
+        :data="data"
+        @legend-click="handleLegendClick"
+      />
+      <cost-line-chart class="max-w-lg w-1/2" ref="lineChart" :data="data" />
+    </div>
+    <div class="justify-center flex items-center">
+      <cost-stacked-chart class="w-2/3" :data="data" />
+    </div>
   </div>
 </template>
 
