ae133bf38808deddf13ef32498e307683fe7a2f9
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-10-11 11:55:32 +0800

    feat: add provider page (#141)

--- frontend/layouts/main.tsx
+++ frontend/layouts/main.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -9,6 +10,10 @@ type Props = {
 const Main: React.FC<Props> = ({ children, title }) => {
   return (
     <div className="flex flex-col h-screen">
+      <Head>
+        <title>DB Cost | RDS & Cloud SQL Instance Pricing Sheet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main className="flex-grow">
         <h1 className="flex flex-row justify-center mx-5 mt-4 text-4xl text-center text-slate-800 space-x-2">
